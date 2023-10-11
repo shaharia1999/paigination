85af7bad10a941e0efe7d24c9957d8eca843787c
shaharia1999 <adeptshaharia@gmail.com>
2023-10-11 22:02:56 +0600

    ReactQuery

--- src/App.js
+++ src/App.js
@@ -6,19 +6,22 @@ import { Route, Routes } from 'react-router-dom';
 import Nested from './Nested';
 import NestedOne from './NestedOne';
 import NestedTwo from './NestedTwo';
+import { QueryClient ,QueryClientProvider} from 'react-query';
 
 function App() {
-
+  const queryClient = new QueryClient()
 
 
   return (
     <div className="App">
+      <QueryClientProvider client={queryClient}>
    <Routes>
       <Route path='/a' element={<Nested></Nested>}>
         <Route path='1' element={<NestedOne/>}></Route>
         <Route path='2' element={<NestedTwo/>}></Route>
       </Route>
    </Routes>
+   </QueryClientProvider>
 {/* 
      {
       currentPage.map((item,index)=>(
--- src/NestedOne.js
+++ src/NestedOne.js
@@ -1,14 +1,49 @@
-import React from 'react';
-import { useOutletContext } from "react-router-dom";
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { useQuery } from 'react-query';
+import { useOutletContext } from 'react-router-dom';
+
 const NestedOne = () => {
-    const [data,setData] = useOutletContext();
-    console.log(data);
-    return (
-        <div>
-            <h1 >one</h1>
-            {/* <h1 onClick={()=>setCount((c) => c + 1)}>one</h1> */}
-        </div>
-    );
+  const [data, setData] = useOutletContext();
+  const [ids, setId] = useState(0);
+
+  useEffect(() => {
+    setId(1);
+  }, []);
+ const  Handele=()=>{
+    
+    setId((prevId) => prevId + 1);
+    console.log(ids);
+  }
+  const fetchData = async () => {
+    
+        try {
+            const response = await axios.get(`https://jsonplaceholder.typicode.com/todos/${ids}`);
+            setData(response.data);
+            console.log(response);
+          } catch (error) {
+            console.error('Error fetching data:', error.message);
+          }
+    
+
+  };
+  const {data:todo,isLoading,isError} = useQuery(['user', ids], fetchData,{
+    enabled: !!ids>0
+  });
+    if(data){
+        console.log(data.title);
+    }
+
+  return (
+    <div>
+      <h1>one</h1>
+      <button onClick={()=>Handele()}>Add</button>
+       {isLoading && <p>Loading...</p>}
+      {isError && <p>Error: </p>} 
+      {/* {todo? "hlw":'hi'} */}
+      {/* Render your data here */}
+    </div>
+  );
 };
 
 export default NestedOne;
